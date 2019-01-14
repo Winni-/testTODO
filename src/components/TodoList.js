1d1e9c11add1e80fe968718e337b97ca365a1f4e
Mihail Pavlov <winni_ne@mail.ru>
2019-01-14 15:35:16 +0300

    Alpha version redux todo example

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Todo } from './Todo'
+
+export const TodoList = ( {todos, toggleTodo} ) => (
+  <ul>
+    { todos.map(todo =>
+      <Todo
+        key={ todo.id }
+        { ...todo }
+        onClick={ () => toggleTodo(todo.id) }
+      />
+    ) }
+  </ul>
+)
